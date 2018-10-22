04eaad6f46a457c70e6606091a387b8c18cbf254
teemukataja <kataja.teemu@gmail.com>
2018-10-22 15:41:19 +0300

    reduce database stress by reducing number of connections

--- api/utils/db_actions.py
+++ api/utils/db_actions.py
@@ -3,120 +3,95 @@
 from ..utils.logging import LOG
 
 
-async def user_exists(user, db_pool):
+async def user_exists(user, connection):
     """Check if user exists."""
     LOG.debug(f'Check if user {user} exists')
-    # Take one connection from the active database connection pool
-    async with db_pool.acquire() as connection:
-        # Start a new session with the connection
-        async with connection.transaction():
-            query = f"""SELECT userid FROM users WHERE userid='{user}';"""
-            statement = await connection.prepare(query)
-            db_response = await statement.fetch()
-            LOG.debug(f'Response: {db_response}')
-            return db_response
+    query = f"""SELECT userid FROM users WHERE userid='{user}';"""
+    statement = await connection.prepare(query)
+    db_response = await statement.fetch()
+    LOG.debug(f'Response: {db_response}')
+    return db_response
 
 
-async def get_dataset_permissions(user, db_pool):
+async def get_dataset_permissions(user, connection):
     """Return dataset permissions for given user."""
     LOG.debug(f'Query database for {user}\'s dataset permissions')
-    # Take one connection from the active database connection pool
-    async with db_pool.acquire() as connection:
-        # Start a new session with the connection
-        async with connection.transaction():
-            query = f"""SELECT organization, a.resid, a.start, a.endt
-                     FROM resource a, entitlement b
-                     WHERE a.id=b.resid
-                     AND b.userid='{user}';"""
-            statement = await connection.prepare(query)
-            db_response = await statement.fetch()
-            LOG.debug(f'Response: {db_response}')
-            return db_response
+    query = f"""SELECT organization, a.resid, a.start, a.endt
+                FROM resource a, entitlement b
+                WHERE a.id=b.resid
+                AND b.userid='{user}';"""
+    statement = await connection.prepare(query)
+    db_response = await statement.fetch()
+    LOG.debug(f'Response: {db_response}')
+    return db_response
 
 
-async def create_user(user, db_pool):
+async def create_user(user, connection):
     """Create new user."""
     LOG.debug(f'Create new user {user}')
-    # Take one connection from the active database connection pool
     try:
-        async with db_pool.acquire() as connection:
-            # Start a new session with the connection
-            async with connection.transaction():
-                await connection.execute(f"""INSERT INTO users VALUES ($1)""", user)
-        return True  # User was created
+        # Make inserts inside of a transaction
+        async with connection.transaction():
+            await connection.execute(f"""INSERT INTO users VALUES ($1)""", user)
+            return True  # User was created
     except Exception as e:
         LOG.debug(f'An error occurred while attempting to create user -> {e}')
         return False
 
 
-# TO DO: Investigate into passing *connection* from create_dataset_permissions(), and not a db_pool
-async def get_dataset_index(ds, db_pool):
+async def get_dataset_index(ds, connection):
     """Check if given dataset exists.
 
     Returns dataset resource id for later use."""
     LOG.debug(f'Check if dataset {ds} exists')
-    # Take one connection from the active database connection pool
-    async with db_pool.acquire() as connection:
-        # Start a new session with the connection
-        async with connection.transaction():
-            query = f"""SELECT id FROM resource WHERE organization='{ds[0]}' AND resid='{ds[1]}';"""
-            statement = await connection.prepare(query)
-            db_response = await statement.fetch()
-            if db_response:
-                LOG.debug(f'Response: {db_response}')
-                return dict(db_response[0])['id']  # Pass id from record
-            else:
-                # Could not find dataset belonging to this organisation
-                return False
+    query = f"""SELECT id FROM resource WHERE organization='{ds[0]}' AND resid='{ds[1]}';"""
+    statement = await connection.prepare(query)
+    db_response = await statement.fetch()
+    if db_response:
+        LOG.debug(f'Response: {db_response}')
+        return dict(db_response[0])['id']  # Pass id from record
+    else:
+        # Could not find dataset belonging to this organisation
+        return False
 
-async def create_dataset_permissions(user, dataset_group, db_pool):
+
+async def create_dataset_permissions(user, dataset_group, connection):
     """Create dataset permissions."""
     LOG.debug('Create dataset permissions.')
-    LOG.debug(dataset_group)
     errors = []
     try:
-        # Take one connection from the active database connection pool
-        async with db_pool.acquire() as connection:
-            # Start a new session with the connection
-            async with connection.transaction():
-                for affiliation in dataset_group:
-                    # aff..[0] = affiliation, aff..[1] = [datasets]
-                    for dataset in affiliation[1]:
-                        dataset_index = await get_dataset_index([affiliation[0], dataset], db_pool)
-                        if dataset_index:
-                            # Dataset belonging to given organisation exists, create permissions for user
-                            await connection.execute(f"""INSERT INTO entitlement (resid, userid) VALUES ($1, $2)""",
-                                                     dataset_index, user)
-                        else:
-                            # Dataset for this organisation doesn't exist
-                            errors.append([affiliation[0], dataset])
-        return errors
+        # Make inserts inside of a transaction and commit all at once
+        async with connection.transaction():
+            for affiliation in dataset_group:
+                # aff..[0] = affiliation, aff..[1] = [datasets]
+                for dataset in affiliation[1]:
+                    dataset_index = await get_dataset_index([affiliation[0], dataset], connection)
+                    if dataset_index:
+                        # Dataset belonging to given organisation exists, create permissions for user
+                        await connection.execute(f"""INSERT INTO entitlement (resid, userid) VALUES ($1, $2)""",
+                                                 dataset_index, user)
+                    else:
+                        # Dataset for this organisation doesn't exist
+                        errors.append([affiliation[0], dataset])
+            return errors
     except Exception as e:
         LOG.debug(f'An error occurred while attempting to create permissions -> {e}')
 
 
-async def remove_dataset_permissions(user, db_pool):
+async def remove_dataset_permissions(user, connection):
     """Remove dataset permissions."""
     LOG.debug('Remove dataset permissions.')
     try:
-        # Take one connection from the active database connection pool
-        async with db_pool.acquire() as connection:
-            # Start a new session with the connection
-            async with connection.transaction():
-                await connection.execute(f"""DELETE FROM entitlement WHERE userid='{user}'""")
+        await connection.execute(f"""DELETE FROM entitlement WHERE userid='{user}'""")
     except Exception as e:
         LOG.debug(f'An error occurred while attempting to remove permissions -> {e}')
 
 
-async def delete_user(user, db_pool):
+async def delete_user(user, connection):
     """Remove user."""
     LOG.debug('Delete user.')
     try:
-        # Take one connection from the active database connection pool
-        async with db_pool.acquire() as connection:
-            # Start a new session with the connection
-            async with connection.transaction():
-                await connection.execute(f"""DELETE FROM users WHERE userid='{user}'""")
+        await connection.execute(f"""DELETE FROM users WHERE userid='{user}'""")
         return True
     except Exception as e:
         LOG.debug(f'An error occurred while attempting to delete user -> {e}')
--- api/utils/process.py
+++ api/utils/process.py
@@ -4,6 +4,7 @@ from ..utils.logging import LOG
 from ..utils.db_actions import user_exists, create_user, delete_user
 from ..utils.db_actions import get_dataset_permissions, create_dataset_permissions, remove_dataset_permissions
 
+
 async def create_response_body(db_response):
     """Construct a dictionary for JSON response body."""
     response_body = {
@@ -22,100 +23,116 @@ async def create_response_body(db_response):
 async def process_post_request(request, db_pool):
     """Create new user with dataset permissions."""
     LOG.debug('Process POST request.')
+    # Put the JSON payload body into a dictionary object
     request_body = await request.json()
-    db_response = await user_exists(request_body['user_identifier'], db_pool)
-    if db_response:
-        # user_identifier exists, new user can't be created
-        return 'Username is taken', False
-    else:
-        # Create new user and dataset permissions
-        user_created = await create_user(request_body['user_identifier'], db_pool)
-        # Wait for user to be created, then add dataset permissions
-        if user_created:
-            # Parse datasets out of request_body
-            # ELIXIR API Specification has a typo in it, but this iterator satisfies it.
-            # Come back and fix this once the specification has been corrected
-            # dataset_group = [[p['affiliation'], p['datasets']] for p in body['permissions']]  # Correct form
-            dataset_group = [[p['affiliation'], p['datasets']] for p in request_body['datasets'][0]['permissions']]
-            permissions_errors = await create_dataset_permissions(request_body['user_identifier'], dataset_group, db_pool)
-            LOG.debug(f'Error list from creating permissions: {permissions_errors}. Should be empty!')
-            if permissions_errors:
-                # If there are errors, return proper message
-                return permissions_errors, True
-            else:
-                # All inserts were processed successfully
-                return None, True
 
+    # Take one connection from the active database connection pool
+    async with db_pool.acquire() as connection:
+        # Check if any users with given name already exist
+        user_found = await user_exists(request_body['user_identifier'], connection)
+        if user_found:
+            # user_identifier exists, new user can't be created
+            return 'Username is taken', False
+        else:
+            # Create new user and dataset permissions
+            user_created = await create_user(request_body['user_identifier'], connection)
+            # Wait for user to be created, then add dataset permissions
+            if user_created:
+                # Parse datasets out of request_body
+                # ELIXIR API Specification has a typo in it, but this iterator satisfies it.
+                # Come back and fix this once the specification has been corrected
+                # dataset_group = [[p['affiliation'], p['datasets']] for p in body['permissions']]  # Correct form
+                dataset_group = [[p['affiliation'], p['datasets']] for p in request_body['datasets'][0]['permissions']]
+                permissions_errors = await create_dataset_permissions(request_body['user_identifier'], dataset_group, connection)
+                LOG.debug(f'Error list from creating permissions: {permissions_errors}. Should be empty!')
+                if permissions_errors:
+                    # If there are errors, return proper message
+                    return permissions_errors, True
+                else:
+                    # All inserts were processed successfully
+                    return None, True
 
 
 async def process_get_request(user, db_pool):
     """Fetch user dataset permissions."""
     LOG.debug('Process GET request.')
-    db_response = await user_exists(user, db_pool)
-    if db_response:
-        permissions = await get_dataset_permissions(user, db_pool)
-        if permissions:
-            # Return permitted datasets
-            permissions_response = await create_response_body(permissions)
-            return None, permissions_response
+
+    # Take one connection from the active database connection pool
+    async with db_pool.acquire() as connection:
+        # Check if any users with given name already exist
+        user_found = await user_exists(user, connection)
+        if user_found:
+            # User was found, fetch permissions
+            permissions = await get_dataset_permissions(user, connection)
+            if permissions:
+                # Return permitted datasets
+                permissions_response = await create_response_body(permissions)
+                return None, permissions_response
+            else:
+                # User has no dataset permissions
+                return 'Permissions for this user not found', False
         else:
-            # User has no dataset permissions
-            return 'Permissions for this user not found', False
-    else:
-        # User doesn't exist
-        return 'User not found', False
+            # User doesn't exist
+            return 'User not found', False
 
 
 async def process_patch_request(user, request, db_pool):
     """Update user's dataset permissions."""
     LOG.debug('Process PATCH request.')
+    # Put the JSON payload body into a dictionary object
     request_body = await request.json()
-    db_response = await user_exists(user, db_pool)
-    if db_response:
-        # user_identifier exists, continue with operations
-        # Check if user has permissions to be removed
-        permissions = await get_dataset_permissions(user, db_pool)
-        if permissions:
-            # Try to remove permissions before adding new permissions
-            await remove_dataset_permissions(user, db_pool)
-        # Parse datasets out of request_body
-        # ELIXIR API Specification has a typo in it, but this iterator satisfies it.
-        # Come back and fix this once the specification has been corrected
-        # dataset_group = [[p['affiliation'], p['datasets']] for p in body['permissions']]  # Correct form
-        dataset_group = [[p['affiliation'], p['datasets']] for p in request_body['datasets'][0]['permissions']]
-        if dataset_group:
-            # If any datasets were listed, add permissions for them
-            permissions_errors = await create_dataset_permissions(user, dataset_group, db_pool)
-            LOG.debug(f'Error list from creating permissions: {permissions_errors}. Should be empty!')
-            if permissions_errors:
-                # If there are errors, return proper message
-                return permissions_errors, True
+
+    # Take one connection from the active database connection pool
+    async with db_pool.acquire() as connection:
+        user_found = await user_exists(user, connection)
+        if user_found:
+            # user_identifier exists, continue with operations
+            # Check if user has permissions to be removed
+            permissions = await get_dataset_permissions(user, connection)
+            if permissions:
+                # Try to remove permissions before adding new permissions
+                await remove_dataset_permissions(user, connection)
+            # Parse datasets out of request_body
+            # ELIXIR API Specification has a typo in it, but this iterator satisfies it.
+            # Come back and fix this once the specification has been corrected
+            # dataset_group = [[p['affiliation'], p['datasets']] for p in body['permissions']]  # Correct form
+            dataset_group = [[p['affiliation'], p['datasets']] for p in request_body['datasets'][0]['permissions']]
+            if dataset_group:
+                # If any datasets were listed, add permissions for them
+                permissions_errors = await create_dataset_permissions(user, dataset_group, connection)
+                LOG.debug(f'Error list from creating permissions: {permissions_errors}. Should be empty!')
+                if permissions_errors:
+                    # If there are errors, return proper message
+                    return permissions_errors, True
+                else:
+                    # All inserts were processed successfully
+                    return None, True
             else:
-                # All inserts were processed successfully
+                # Body contained no datasets, end operations here (permissions removed, none added)
                 return None, True
         else:
-            # Body contained no datasets, end operations here
-            return None, True        
-    else:
-        return 'User not found', False
+            return 'User not found', False
 
 
 async def process_delete_request(user, db_pool):
     """Fetch user dataset permissions."""
     LOG.debug('Process GET request.')
-    db_response = await user_exists(user, db_pool)
-    if db_response:
-        # Check if user has permissions to be removed
-        permissions = await get_dataset_permissions(user, db_pool)
-        if permissions:
-            # Try to remove permissions before removing user
-            await remove_dataset_permissions(user, db_pool)
-        # Finally remove the user
-        user_removed = await delete_user(user, db_pool)
-        if user_removed:
-            return None, True
+
+    # Take one connection from the active database connection pool
+    async with db_pool.acquire() as connection:
+        user_found = await user_exists(user, connection)
+        if user_found:
+            # Check if user has permissions to be removed
+            permissions = await get_dataset_permissions(user, connection)
+            if permissions:
+                # Try to remove permissions before removing user
+                await remove_dataset_permissions(user, connection)
+            # Finally remove the user
+            user_removed = await delete_user(user, connection)
+            if user_removed:
+                return None, True
+            else:
+                return 'Database error when attempting to remove user', False
         else:
-            return 'Database error when attempting to remove user', False
-    else:
-        # User doesn't exist
-        return 'User not found', False
+            # User doesn't exist
+            return 'User not found', False
