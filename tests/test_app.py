aaa3b02ef22dc6545807932e4264d62c4475c0e5
teemukataja <kataja.teemu@gmail.com>
2018-10-25 13:15:16 +0300

    #3 #4 #5 try another test method, modify tox and include coveralls

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ import unittest
 
 import asynctest
 
-from aiohttp.test_utils import AioHTTPTestCase
+from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
 from aiohttp import web
 from api.app import init_app, main
 from unittest import mock
@@ -19,19 +19,20 @@ class TestFunctions(unittest.TestCase):
         """Clear test case."""
         pass
 
-    async def test_db_pool_init(self):
-        """Test opening of db pool."""
-        app = {}
-        with asynctest.mock.patch('api.app.init_db_pool') as db_mock:
-            await init_app(app)
-            await db_mock.assert_called()
+    # REDO!!
+    # async def test_db_pool_init(self):
+    #     """Test opening of db pool."""
+    #     app = {}
+    #     with asynctest.mock.patch('api.app.init_db_pool') as db_mock:
+    #         await init_app(app)
+    #         db_mock.assert_called()
 
-    async def test_db_pool_close(self):
-        """Test closing of db pool."""
-        app = {}
-        with asynctest.mock.patch('api.app.init_db_pool') as db_mock:
-            await init_app(app)
-            await db_mock.close.assert_called()
+    # async def test_db_pool_close(self):
+    #     """Test closing of db pool."""
+    #     app = {}
+    #     with asynctest.mock.patch('api.app.init_db_pool') as db_mock:
+    #         await init_app(app)
+    #         db_mock.close.assert_called()
 
     @mock.patch('api.app.web')
     def test_main(self, mock_webapp):
@@ -52,11 +53,18 @@ class TestEndpoints(AioHTTPTestCase):
         """Get the server initialisation."""
         return init_app()
 
-    async def test_health(self):
-        """Test that the server is running."""
-        response = await self.client.request("GET", "/")
-        assert response.status == 200
-        assert response.text == 'ELIXIR AAI API for REMS'
+    # async def test_health(self):
+    #     """Test that the server is running."""
+    #     response = await self.client.request("GET", "/")
+    #     assert response.status == 200
+    #     assert response.text == 'ELIXIR AAI API for REMS'
+
+    @unittest_run_loop
+    async def test_info(self):
+        """Test the info endpoint."""
+        with asynctest.mock.patch('api.app.api_get', side_effect={"smth": "value"}):
+            resp = await self.client.request("GET", "/")
+        assert 200 == resp.status
 
 
 if __name__ == '__main__':
