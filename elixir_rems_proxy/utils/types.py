1fcb4c99c0c1bef3d84efd57d9147bdd48b26574
MalinAhlberg <malin.ahlberg@nbis.se>
2020-08-24 11:28:11 +0200

    Cleanup unused imports

--- elixir_rems_proxy/utils/types.py
+++ elixir_rems_proxy/utils/types.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, NewType, Union
+from typing import Dict, NewType, Union
 
 Passport = NewType("Passport", str)  # an encoded list of visas
 Permission = NewType("Permission", dict)
