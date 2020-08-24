4e81f17da6ce1ac8b17dab89a5d5c17552848a54
teemu.kataja <teemu.kataja@csc.fi>
2020-08-24 13:05:33 +0300

    use explicit argument keywords in config and move config class to types

--- elixir_rems_proxy/config/__init__.py
+++ elixir_rems_proxy/config/__init__.py
@@ -8,23 +8,13 @@ import logging
 from pathlib import Path
 from configparser import ConfigParser
 from distutils.util import strtobool
-from typing import NamedTuple, Union
+from typing import Union
+
+from ..utils.types import Config
 
 formatting = "[%(asctime)s][%(name)s][%(process)d %(processName)s][%(levelname)-8s] (L:%(lineno)s) %(module)s | %(funcName)s: %(message)s"
 logging.basicConfig(level=logging.DEBUG if bool(strtobool(os.environ.get("DEBUG", "False"))) else logging.INFO, format=formatting)
 LOG = logging.getLogger("ELIXIR")
-
-
-class Config(NamedTuple):
-    """The app configuration."""
-
-    rems_url: str
-    repository: str
-    jwt_exp: int
-    public_key: dict
-    private_key: dict
-    host: str = "0.0.0.0"
-    port: Union[int, str] = 8080
 
 
 def load_json_file(path: Union[str, Path]) -> dict:
@@ -43,13 +33,13 @@ def parse_config_file(path: Union[str, Path]) -> Config:
     config.read(path)
 
     return Config(
-        os.environ.get("REMS_URL", config.get("rems", "rems_url")),
-        os.environ.get("GA4GH_REPOSITORY", config.get("ga4gh", "repository")),
-        int(os.environ.get("JWT_EXP", config.get("ga4gh", "jwt_exp"))),
-        load_json_file(os.environ.get("JWK_PUBLIC_KEY_FILE", Path(__file__).resolve().parent.joinpath("public_key.json"))),
-        load_json_file(os.environ.get("JWK_PRIVATE_KEY_FILE", Path(__file__).resolve().parent.joinpath("private_key.json"))),
-        os.environ.get("APP_HOST", config.get("server", "host")),
-        os.environ.get("APP_PORT", config.get("server", "port")),
+        rems_url=os.environ.get("REMS_URL", config.get("rems", "rems_url")),
+        repository=os.environ.get("GA4GH_REPOSITORY", config.get("ga4gh", "repository")),
+        jwt_exp=int(os.environ.get("JWT_EXP", config.get("ga4gh", "jwt_exp"))),
+        public_key=load_json_file(os.environ.get("JWK_PUBLIC_KEY_FILE", Path(__file__).resolve().parent.joinpath("public_key.json"))),
+        private_key=load_json_file(os.environ.get("JWK_PRIVATE_KEY_FILE", Path(__file__).resolve().parent.joinpath("private_key.json"))),
+        host=os.environ.get("APP_HOST", config.get("server", "host")),
+        port=os.environ.get("APP_PORT", config.get("server", "port")),
     )
 
 
--- elixir_rems_proxy/utils/types.py
+++ elixir_rems_proxy/utils/types.py
@@ -1,5 +1,17 @@
-from typing import Dict, NewType, Union
+from typing import Dict, NewType, Union, NamedTuple
 
 Passport = NewType("Passport", str)  # an encoded list of visas
 Permission = NewType("Permission", dict)
 Visa = NewType("Visa", Dict[str, Union[str, int, None]])
+
+
+class Config(NamedTuple):
+    """The app configuration."""
+
+    rems_url: str
+    repository: str
+    jwt_exp: int
+    public_key: dict
+    private_key: dict
+    host: str = "0.0.0.0"
+    port: Union[int, str] = 8080
