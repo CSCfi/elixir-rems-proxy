013dcf20d851768b092cde582f9e6efd32de8aa1
MalinAhlberg <malin.ahlberg@nbis.se>
2020-09-07 10:23:29 +0200

    Cleanup of docstrings, names etc

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,11 +5,11 @@ from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
 from elixir_rems_proxy.app import init_app
 
 
-class TestBasicFunctions(AioHTTPTestCase):
-    """Test functions for generation passports and visas."""
+class TestApp(AioHTTPTestCase):
+    """Test functions for the web application."""
 
     async def get_application(self):
-        """Retrieve web Application for test."""
+        """Retrieve web app for the tests."""
         return await init_app()
 
     @unittest_run_loop
@@ -27,7 +27,7 @@ class TestBasicFunctions(AioHTTPTestCase):
     @asynctest.patch("elixir_rems_proxy.app.request_rems_permissions", return_value=[])
     @unittest_run_loop
     async def test_permissions(self, _rems_permissions):
-        """Test that /permissions requires an api key."""
+        """Test that /permissions works if api key is provided."""
         resp = await self.client.request("GET", "/permissions/user", headers={"Permissions-Api-Key": "abc"})
         self.assertEqual(200, resp.status)
         # check that a json dict is returned
--- tests/test_visas.py
+++ tests/test_visas.py
@@ -1,5 +1,5 @@
 import aiohttp
-import asynctest  # TODO require
+import asynctest
 import random
 
 from authlib.jose import jwt
@@ -7,11 +7,10 @@ from asynctest import CoroutineMock
 from unittest.mock import patch
 
 from elixir_rems_proxy.config import CONFIG
-from elixir_rems_proxy.endpoints.permissions import create_ga4gh_visa_v1, create_ga4gh_passports, iso_to_timestamp, call_rems_api
-import elixir_rems_proxy.endpoints.permissions
+import elixir_rems_proxy.endpoints.permissions as permissions
 
 
-class TestBasicFunctions(asynctest.TestCase):
+class TestPermissionFunctions(asynctest.TestCase):
     """Test functions for generation passports and visas."""
 
     @patch("elixir_rems_proxy.endpoints.permissions.iso_to_timestamp", return_value="")
@@ -19,7 +18,7 @@ class TestBasicFunctions(asynctest.TestCase):
         """Test visa generation."""
         # Test that you get as many items in your visa as the input permissions.
         length = random.randint(1, 10)
-        visas = await create_ga4gh_visa_v1([{"start": 1} for x in range(length)])
+        visas = await permissions.create_ga4gh_visa_v1([{"start": 1} for x in range(length)])
         self.assertEqual(length, len(visas), msg=f"Unequal length for input {length}")
 
         # Test that the first one contains all fields.
@@ -38,10 +37,9 @@ class TestBasicFunctions(asynctest.TestCase):
         class Request:
             host = "dummyhost"
 
-        passport = await create_ga4gh_passports(Request(), user, visas)
+        passport = await permissions.create_ga4gh_passports(Request(), user, visas)
         # Check the length of the passport (number of visas)
         self.assertEqual(length, len(passport), msg=f"Unequal length for input {length}")
-
         # Check that the first visa can be decoded
         visa = passport[0]
         decoded = jwt.decode(visa, CONFIG.public_key)
@@ -56,43 +54,43 @@ class TestBasicFunctions(asynctest.TestCase):
         """Test that timestamp generation works as expected."""
         iso = "2020-01-01T12:00:00.000Z"
         expected_stamp = 1577880000  # TODO not as in docstring (should it be?)
-        stamp = await iso_to_timestamp(iso)
+        stamp = await permissions.iso_to_timestamp(iso)
         self.assertEqual(stamp, expected_stamp)
 
     async def test_generate_jtw_timestamp(self):
         """Test that the expires time is in the future."""
-        iat, exp = await elixir_rems_proxy.endpoints.permissions.generate_jwt_timestamps()
+        iat, exp = await permissions.generate_jwt_timestamps()
         self.assertLess(iat, exp)
 
     @asynctest.patch("aiohttp.ClientSession.get")
     async def test_call_api_ok(self, session_mock):
-        """Test that a successfull call to the rems api."""
+        """Test that a successfull call to the rems api results in a response."""
 
         session_mock.return_value.__aenter__.return_value.json = CoroutineMock(side_effect=[{"test": "test"}])
         session_mock.return_value.__aenter__.return_value.status = 200
 
-        res = await call_rems_api("url", {})
+        res = await permissions.call_rems_api("url", {})
         # if successfull, the json response should be returned without changes
         self.assertEqual(res, {"test": "test"})
 
     @asynctest.patch("aiohttp.ClientSession.get")
     async def test_call_api_fail(self, session_mock):
-        """Test that an unsuccessfull call to the rems api."""
+        """Test that an unsuccessfull call to the rems api raises an error."""
 
         session_mock.return_value.__aenter__.return_value.status = 400
         # status code 400 should raise bad request
         with self.assertRaises(aiohttp.web_exceptions.HTTPBadRequest) as cm:
-            await call_rems_api("url", {})
+            await permissions.call_rems_api("url", {})
         self.assertEqual(cm.exception.status, 400)
 
     @asynctest.patch("aiohttp.ClientSession.get")
     async def test_call_api_error(self, session_mock):
-        """Test that an unsuccessfull call to the rems api."""
+        """Test that an unsuccessfull call to the rems api raises an error."""
 
         session_mock.return_value.__aenter__.return_value.status = 500
-        # status code 400 should internal server error
+        # status code 500 should internal server error
         with self.assertRaises(aiohttp.web_exceptions.HTTPInternalServerError) as cm:
-            await call_rems_api("url", {})
+            await permissions.call_rems_api("url", {})
         self.assertEqual(cm.exception.status, 500)
 
     @asynctest.patch("elixir_rems_proxy.endpoints.permissions.create_ga4gh_visa_v1")
@@ -102,7 +100,7 @@ class TestBasicFunctions(asynctest.TestCase):
         """Test that no passports are returned if no permissions are given."""
 
         mock_call_api.return_value = []
-        passports = await elixir_rems_proxy.endpoints.permissions.request_rems_permissions("", "", "")
+        passports = await permissions.request_rems_permissions("", "", "")
         self.assertEqual(passports, [])
 
     @asynctest.patch("elixir_rems_proxy.endpoints.permissions.create_ga4gh_visa_v1")
