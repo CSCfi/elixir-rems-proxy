71ec901fadb6fb30d05faf58899e09aff9844c6a
MalinAhlberg <malin.ahlberg@nbis.se>
2020-09-07 13:10:26 +0200

    Move test deps to extras_require

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setup(
     ],
     package_data={"": ["*.ini", "*.json"]},
     install_requires=["aiohttp", "authlib", "cryptography", "uvloop", "gunicorn"],
+    extras_require={"test": ["asynctest", "pytest", "pytest-cov"]},
     entry_points={"console_scripts": ["start_elixir_rems_proxy=elixir_rems_proxy.app:main"]},
 )
