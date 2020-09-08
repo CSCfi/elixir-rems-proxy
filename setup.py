5d2a0a5269a2905257c3955411c1fff00d7eab32
Teemu Kataja <teemu.kataja@csc.fi>
2020-09-08 11:49:08 +0300

    stable version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="elixir_rems_proxy",
-    version="0.2.dev",
+    version="1.0.0",
     description="ELIXIR Permissions API proxy for REMS API",
     long_description_content_type="text/markdown",
     project_urls={"Source": "https://github.com/CSCfi/elixir-rems-proxy"},
