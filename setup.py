afd98c776ae1123932e06278197dada3431bf9f9
Teemu Kataja <teemu.kataja@csc.fi>
2020-09-14 11:09:53 +0300

    Bump patch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="elixir_rems_proxy",
-    version="1.0.0",
+    version="1.0.1",
     description="ELIXIR Permissions API proxy for REMS API",
     long_description_content_type="text/markdown",
     project_urls={"Source": "https://github.com/CSCfi/elixir-rems-proxy"},
